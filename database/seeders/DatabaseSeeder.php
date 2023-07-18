c35ce1fe1721526f97c693f11ffbd62717315b31
dustisme <androsrivas@outlook.com>
2023-07-18 09:49:21 +0200

    changed admin user creatation into role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
-use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Contracts\Role;
+use Illuminate\Support\Facades\Log;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,15 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(RoleSeeder::class);
         
-        User::create([
-            'email' => 'admin@example.com', 
-            'nickname' => 'admin',
-            'email_verified_at' => now(),
-            'password' => bcrypt('admin123'),
-            'remember_token' => Str::random(10),
-        ])->assignRole('admin');
+        $this->call(RoleSeeder::class);
 
         User::factory(5)->create();
     }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class RoleSeeder extends Seeder
@@ -14,25 +17,31 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        $admin = Role::create(['name' => 'admin']);
-        $player = Role::create(['name' => 'player']);
+        $admin = Role::create(['name' => 'admin', 'guard_name' => 'api']);
+        $player = Role::create(['name' => 'player', 'guard_name' => 'api']);
 
-        //admin routes
-        Permission::create(['name' => 'admin.players'])->assignRole($admin);
-        Permission::create(['name' => 'admin.ranking'])->assignRole($admin);
-        Permission::create(['name' => 'admin.ranking.winner'])->assignRole('admin');
-        Permission::create(['name' => 'admin.ranking.loser'])->assignRole('admin');
-        Permission::create(['name' => 'admin.player.delete'])->assignRole($admin);
+        //admin permissions
+        Permission::create(['name' => 'admin.players', 'guard_name' => 'api'])->assignRole($admin);
+        Permission::create(['name' => 'admin.ranking', 'guard_name' => 'api'])->assignRole($admin);
+        Permission::create(['name' => 'admin.ranking.winner', 'guard_name' => 'api'])->assignRole($admin);
+        Permission::create(['name' => 'admin.ranking.loser', 'guard_name' => 'api'])->assignRole($admin);
+        Permission::create(['name' => 'admin.player.delete', 'guard_name' => 'api'])->assignRole($admin);
 
-        //players routes
-        Permission::create(['name' => 'players.update'])->assignRole($player);
-        Permission::create(['name' => 'players.games.delete'])->assignRole($player);
+        //players permissions
+        Permission::create(['name' => 'players.update', 'guard_name' => 'api'])->assignRole($player);
+        Permission::create(['name' => 'players.games.delete', 'guard_name' => 'api'])->assignRole($player);
         
-        //admin and players routes
-        Permission::create(['name' => 'users.login'])->syncRoles($admin, $player);
-        Permission::create(['name' => 'players.games'])->syncRoles($admin, $player);
-        Permission::create(['name' => 'players.games.newGame'])->syncRoles($admin, $player);
-        
-        
+        //admin and players permissions
+        Permission::create(['name' => 'players.games', 'guard_name' => 'api'])->syncRoles($admin, $player);
+        Permission::create(['name' => 'players.games.newGame', 'guard_name' => 'api'])->syncRoles($admin, $player);
+
+        //user with admin role
+        User::create([
+            'email' => 'admin@example.com', 
+            'nickname' => 'admin',
+            'email_verified_at' => now(),
+            'password' => bcrypt('admin123'),
+            'remember_token' => Str::random(10),
+        ])->assignRole('admin');
     }
 }
