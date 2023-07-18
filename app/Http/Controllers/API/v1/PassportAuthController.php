22fa5d0b46fffe92551c5b083238994da65dfc56
dustisme <androsrivas@outlook.com>
2023-07-18 09:43:09 +0200

    user query to find nickname changed (LIKE eliminated)

--- app/Http/Controllers/API/v1/PassportAuthController.php
+++ app/Http/Controllers/API/v1/PassportAuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 
@@ -15,82 +16,82 @@ class PassportAuthController extends Controller
 {
     public function register(Request $request)
     {
-        $validator = Validator::make( $request->all(), [
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email|max:35',
             'password' => 'required|string|min:8',
-            'nickname' => 'nullable|string|min:3|max:10',
+            'nickname' => 'nullable|string|min:3',
         ]);
-        
-        if ( $validator->fails()) {
+
+        if ($validator->fails()) {
 
             return response()->json(['errors' => $validator->errors()->all()], 422);
-
         }
 
         $emailExists = User::where('email', $request->email)->first();
-        $nicknameExists = User::where('nickname', 'LIKE', $request->nickname)->first();
+        $nicknameExists = User::where('nickname', $request->nickname)->first();
 
-        if($emailExists) {
-            
+        if ($emailExists) {
+
             return response()->json([
                 'message' => 'This email already exists.',
             ], 400);
-
         } else if ($nicknameExists) {
 
             return response()->json([
                 'message' => 'This nickname is already taken.',
             ], 400);
-
         } else {
 
-        $user = User::create([
-            'nickname' => $request->nickname ?: 'anonymous-' . rand(100,999),
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-            'email_verified_at' => now(),
-            'remember_token' => Str::random(10), 
-        ]);
+            $user = User::create([
+                'nickname' => $request->nickname ?: 'anonymous-' . rand(100, 999),
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+            ]);
 
-        $accessToken = $user->createToken('register')->accessToken;
+            $accessToken = $user->createToken('register')->accessToken;
 
-        return response()
-            ->json([
-                'message' => 'You have been registered successfully.',
-                'user' => $user,
-                'access_token' => $accessToken,
-            ], 200);
+            return response()
+                ->json([
+                    'message' => 'You have been registered successfully.',
+                    'user' => UserResource::make($user),
+                    'access_token' => $accessToken,
+                ], 200);
         }
     }
 
     public function login(Request $request)
     {
-        $data = [
-            'email' => $request->email,
-            'password' => $request->password,
-        ];
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
 
-        if (auth()->attempt($data)) {
+        if ($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
+        }
 
-            $token = auth()->user()->createToken('login')->accessToken;
-            
-            return response()
-                ->json([
-                    'message' => 'Logged in successfully.',
-                    'user' => Auth::user(),
-                    'token' => $token,
-                ], 200);
+        $user = User::where('email', $request->email)->first();
+
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+
+                $token = $user->createToken('login')->accessToken;
+
+                return response()->json([
+                    'message' => 'Logged in succesfully',
+                    'user' => UserResource::make($user),
+                    'token' => $token], 200);
+
+            } else {
+
+                return response()->json(['message' => 'Incorrect password.'], 422);
+            }
 
         } else {
 
-            return response()->json(['error' => 'Unauthorised'], 401);
-
+            return response(['message' => 'User does not exist.'], 422);
         }
-    }
-
-    public function userInfo()
-    {
-        $user = auth()->user();
-        return response()->json(['user' => $user], 200);
     }
 }
